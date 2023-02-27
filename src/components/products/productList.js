9d64c9cac948fb14c1171dc144885c8b06478cc9
Myra Smith <myra.fowlkes.smith@gmail.com>
2023-02-27 09:30:53 -0500

    modifications to make similar to the lab

--- src/components/products/productList.js
+++ src/components/products/productList.js
@@ -1,24 +1,38 @@
+export default function ProductList({ products, type }) {
+  const [lamps, candles] = products.reduce(
+    ([lamps, candles], product) => {
+      if (product.category === "lamp") {
+        return [[...lamps, product], candles];
+      } else {
+        return [lamps, [...candles, product]];
+      }
+    },
+    [[], []]
+  );
 
-
-export default function ProductList({products, type}) {
-    return (
-        <section><h2>All {type}</h2>
-        <article className="products">
-            <ul>
-                {
-                    products.map(product => {
-                        return (
-                            <li key={product.id}>
-                                <h4>{product.name}</h4>
-                                <img src={product.image} alt={product.name} />
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-
-
-        </article>
-        </section>
-    );
-  };
+  return (
+    <section>
+      <h2>All {type}</h2>
+      <article className="products">
+        <ul>
+          {lamps.map((product) => {
+            return (
+              <li key={product.id}>
+                <h4>{product.name}</h4>
+                <img src={product.image} alt={product.name} />
+              </li>
+            );
+          })}
+          {candles.map((product) => {
+            return (
+              <li key={product.id}>
+                <h4>{product.name}</h4>
+                <img src={product.image} alt={product.name} />
+              </li>
+            );
+          })}
+        </ul>
+      </article>
+    </section>
+  );
+}
