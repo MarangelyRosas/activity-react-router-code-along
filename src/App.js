9eacbe5d1d057ce881a9a313fa3b046e066429f3
Marangely Rosas <Ladykaliber22@gmail.com>
2023-02-28 01:18:46 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import Footer from "./components/common/footer.js";
-import Header from "./components/common/header";
-import Nav from "./components/common/nav";
-import About from "./components/pages/about";
-import Home from "./components/pages/home";
-import Newsletter from "./components/pages/newsletter";
-import ProductList from "./components/products/productList";
-import Product from "./components/products/product";
+import Footer from './components/common/footer.js'
+import Header from './components/common/header'
+import Nav from './components/common/nav'
+import About from './components/pages/about'
+import Home from './components/pages/home'
+import Newsletter from './components/pages/newsletter'
+import ProductList from './components/products/productList'
+import Product from './components/products/product'
 
-import lamps from "./data/lamps.json";
-import candles from "./data/candles.json";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // this is a named export
+import lamps from './data/lamps.json'
+import candles from './data/candles.json'
 
-const allProducts = lamps.concat(candles);
+
+const allProducts = lamps.concat(candles)
 
 function App() {
   return (
     <div className="App">
+       <Router>
       <Header />
       <div className="wrapper">
-        <Nav />
-        <main>
-          <Home />
-          <About />
-          <Newsletter />
-          <ProductList products={allProducts} type={"Lamps and Candles"} />
-        </main>
-      </div>
-      <Footer />
+       
+      <Nav />
+      <main> // only content in our main tag that's going to switch depending on which route we are viewing
+        <Routes>
+         <Route path="/" element={<Home/>} />
+         <Route path="/about" element={<About/>} />
+         <Route path="/products/*" element={<ProductList products={allProducts} type={"Lamps and Candles"} />} />
+         <Route path="/newsletter" element={<Newsletter />} />
+        {/* <Home/>
+        <About/> */}
+        {/* <Newsletter />
+        <ProductList products={lamps} type={'Lamps'}/>
+        <ProductList products={candles} type={'Candles'}/> */} 
+        </Routes>
+        
+      </main>
+    </div>
+    <Footer />
+    </Router>
     </div>
   );
 }
--- src/components/products/productList.js
+++ src/components/products/productList.js
@@ -1,3 +1,7 @@
+// Rendered when my url looks like : /products OR /products/candles OR /products/lamps
+
+import { Routes, Route, Navigate} from  "react-router-dom";
+
 export default function ProductList({ products, type }) {
   const [lamps, candles] = products.reduce(
     ([lamps, candles], product) => {
@@ -10,27 +14,45 @@ export default function ProductList({ products, type }) {
     [[], []]
   );
 
+const lampsList = lamps.map((product) => {
+  return (
+    <li key={product.id}>
+      <h4>{product.name}</h4>
+      <img src={product.image} alt={product.name} />
+    </li>
+  );
+});
+
+const candlesList = candles.map((product) => {
+  return (
+    <li key={product.id}>
+      <h4>{product.name}</h4>
+      <img src={product.image} alt={product.name} />
+    </li>
+  );
+});
+
   return (
     <section>
       <h2>All {type}</h2>
       <article className="products">
         <ul>
-          {lamps.map((product) => {
+          {/* {lamps.map((product) => {
             return (
               <li key={product.id}>
                 <h4>{product.name}</h4>
                 <img src={product.image} alt={product.name} />
               </li>
             );
-          })}
-          {candles.map((product) => {
-            return (
-              <li key={product.id}>
-                <h4>{product.name}</h4>
-                <img src={product.image} alt={product.name} />
-              </li>
-            );
-          })}
+          })} */}
+
+        {/* path="/products/candles" path="/candles" path="candles" */}
+        <Routes>
+          <Route path="candles" element={<>{candlesList}</>} /> {" "} {/* FOR CANDLES */}
+          <Route path="lamps" element={<>{lampsList}</>} /> {" "} {/* FOR LAMPS */}
+          {/* How can we re-direct users from /products TO /products/lamps??? */}
+          <Route path="/" element={<Navigate to="/products/lamps" />} />
+        </Routes>
         </ul>
       </article>
     </section>
